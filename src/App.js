49eecee6e7665861fb5aba4fc1daf11d946951c0
susmita sahu <suslipi.sahu@gmail.com>
2025-02-16 21:31:51 +0530

    netflix-gpp firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-green-500">
-      Namaste!! India !!
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
