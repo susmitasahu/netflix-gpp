736cd1c24e12f15d6ff61b14f261ec63ae7350da
susmita sahu <suslipi.sahu@gmail.com>
2025-02-23 21:39:04 +0530

    netflix mainContainer trailer with auto play

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import Body from "./components/Body";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
-function App() {
-  return <Body />;
-}
+const App = () =>
+  <Provider store={appStore}>
+    <Body />
+  </Provider>;
 
 export default App;
